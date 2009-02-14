90655f596b461bc5819f8e9d263f481cbe96a582
adam.sah@gmail.com <adam.sah@gmail.com@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-02-14 00:47:10 +0000

    added multiple <location>s and <dateTimeDuration>s, for testing purposes

--- datahub/footprint_lib.py
+++ datahub/footprint_lib.py
@@ -7,7 +7,8 @@ import os
 from pytz import timezone
 import pytz
 
-SEP = "\t"
+FIELDSEP = "\t"
+RECORDSEP = "\n"
 
 def getTagValue(entity, tag):
   #print "----------------------------------------"
@@ -35,6 +36,8 @@ def cvtDateTimeToGoogleBase(datestr, timestr, tz):
   # timestr = HH:MM:SS
   # tz = America/New_York
   #print "datestr="+datestr+" timestr="+timestr+" tz="+tz
+  if (datestr == ""):
+    return ""
   if (timestr == ""):
     ts = datetime.strptime(datestr, "%Y-%m-%d")
   else:
@@ -48,52 +51,150 @@ def cvtDateTimeToGoogleBase(datestr, timestr, tz):
   res = ts.strftime("%Y-%m-%dT%H:%M:%S")
   return res
 
+csv_repeated_fields = ['categories','audiences',]
+direct_map_fields = ['opportunityID','organizationID','abstract','volunteersSlots','volunteersFilled','volunteersStillNeeded','minimumAge','sexRestrictedTo','skills','contactName','contactPhone','contactEmail','providerURL','language','lastUpdated','expires',]
+organization_fields = ['nationalEIN','guidestarID','name','missionStatement','description','phone','fax','email','organizationURL','logoURL','providerURL',]
+def value(n):
+  if (n.firstChild != None):
+    return n.firstChild.data.replace(",", "")
+  else:
+    return ""
+
+def flattenFieldToCSV(field):
+  #print field
+  return ",".join(filter(lambda x: x != "", map(value, field.childNodes)))
+
+debug = False
+printhead = False
+def outputField(name, value):
+  global printhead
+  if printhead == True:
+    return name
+  if debug:
+    if (len(value) > 70):
+      value = value[0:67] + "..."
+    return name.rjust(22) + " : " + value
+  return value
+
+def outputLocationField(node, mapped_name):
+  # note: avoid commas, so it works with CSV
+  # (this is good enough for the geocoder)
+  loc = getTagValue(node, "city") + " "
+  loc += getTagValue(node, "region") + " " 
+  loc += getTagValue(node, "postalCode")
+  return outputField(mapped_name, loc)
+
+
+def outputTagValue(node, fieldname):
+  return outputField(fieldname, getTagValue(node, fieldname))
+
+def getDirectMappedField(opp, org):
+  s = FIELDSEP
+  paid = getTagValue(opp, "paid")
+  if (paid == "" or paid.lower()[0] != "y"):
+    paid = "n"
+  else:
+    paid = "y"
+  s += outputField("paid", paid)
+  for field in direct_map_fields:
+    s += FIELDSEP
+    s += outputTagValue(opp, field)
+  for field in organization_fields:
+    s += FIELDSEP
+    s += outputTagValue(org, field)
+  for field in csv_repeated_fields:
+    s += FIELDSEP
+    l = opp.getElementsByTagName(field)
+    if (l.length > 0):
+      s += outputField(field,flattenFieldToCSV(l[0]))
+    else:
+      s += ""
+  # orgLocation
+  s += FIELDSEP
+  l = opp.getElementsByTagName(field)
+  if (l.length > 0):
+    s += outputLocationField(l[0], "orgLocation")
+  else:
+    s += outputField("orgLocation", "")
+  return s
+
+# these are fields that exist in other Base schemas-- for the sake of
+# possible syndication, we try to make ourselves look like other Base
+# feeds.  Since we're talking about a small overlap, these fields are
+# populated *as well as* direct mapping of the footprint XML fields.
+#
+# gender - The gender of the individual.
+# quantity - Number. Indicate a value of 0 for out-of-stock items.
+# job_function - The function of the employment position., e.g. Product Manager
+# employer - The company providing employment.
+# publish_date - The date the job was published.
+# 
+def getBaseOtherFields(opp, org):
+  s = FIELDSEP
+  # gender
+  s += outputTagValue(opp, "sexRestritedTo")
+  # quantity
+  s += FIELDSEP
+  s += outputTagValue(opp, "volunteersStillNeeded")
+  # employer
+  s += FIELDSEP
+  s += outputTagValue(org, "name")
+  # publish_date
+  # TODO: what tz is lastUpdated?
+  s += FIELDSEP
+  pubdate = getTagValue(opp, "lastUpdated")
+  tz = ""
+  if (pubdate == ""):
+    startDate = getTagValue(opp, "startDate")
+    startTime = getTagValue(opp, "startTime")
+    tz = getTagValue(opp, "tzOlsonPath")
+    pubdate = cvtDateTimeToGoogleBase(startDate, startTime, tz)
+  s += outputField("publish_date", pubdate)
+  # expiration_date, in PST
+  s += FIELDSEP
+  expires = getTagValue(opp, "expires")
+  # TODO: what tz is expires?
+  expires = cvtDateTimeToGoogleBase(expires, "", tz)
+  s += outputField("expires", expires)
+  return s
+
 def getBaseEventRequiredFields(opp, org):
+  s = ""
+
   # title
-  s = getTagValue(opp, "title")
+  s += outputTagValue(opp, "title")
+  s += FIELDSEP
 
   # description
-  s += SEP
-  s += getTagValue(opp, "description")
+  s += outputTagValue(opp, "description")
 
   # link
-  s += SEP
+  s += FIELDSEP
   url = getTagValue(opp, "providerURL")
   if (url == ""):
     url = getTagValue(org, "providerURL")
   if (url == ""):
     url = getTagValue(org, "organizationURL")
-  s += url
+  s += outputField("link", url)
 
-  # location.  note: avoid commas, so it works with CSV
-  # (this is good enough for the geocoder)
-  s += SEP
-  s += getTagValue(opp, "city") + " "
-  s += getTagValue(opp, "region") + " "
-  s += getTagValue(opp, "postalCode")
-
-  # event_time_range
-  # e.g. 2006-12-20T23:00:00/2006-12-21T08:30:00, in PST (GMT-8)
-  s += SEP
-  startDate = getTagValue(opp, "startDate")
-  startTime = getTagValue(opp, "startTime")
-  endDate = getTagValue(opp, "endDate")
-  endTime = getTagValue(opp, "endTime")
-  tz = getTagValue(opp, "tz_olson_path")
-  # e.g. 2006-12-20T23:00:00/2006-12-21T08:30:00, in PST (GMT-8)
-  if (startDate == ""):
-    s += ""
-  elif (endDate == ""):
-    s += cvtDateTimeToGoogleBase(startDate, startTime, tz)
-  else:
-    s += cvtDateTimeToGoogleBase(startDate, startTime, tz)
-    s += "/"
-    s += cvtDateTimeToGoogleBase(endDate, endTime, tz)
+  # event_type
+  s += FIELDSEP
+  s += outputField("event_type", "volunteering")
 
   return s
 
 
 def convertToGoogleBaseEventsType(xmldoc):
+  s = ""
+  global printhead, debug
+  recno = 0
+
+  printhead = True
+  s += getBaseEventRequiredFields(xmldoc, xmldoc)
+  s += getBaseOtherFields(xmldoc, xmldoc)
+  s += getDirectMappedField(xmldoc, xmldoc)
+  printhead = False
+
   organizations = xmldoc.getElementsByTagName("Organization")
   known_orgs = {}
   for org in organizations:
@@ -101,33 +202,54 @@ def convertToGoogleBaseEventsType(xmldoc):
     if (id != ""):
       known_orgs[id] = org
     
-  s = "this is the header line\n"
   opportunities = xmldoc.getElementsByTagName("Opportunity")
   for opp in opportunities:
     id = getTagValue(opp, "opportunityID")
     if (id == ""):
+      print "no opportunityID"
       continue
     org_id = getTagValue(opp, "organizationID")
     if (org_id not in known_orgs):
       print "unknown org_id: " + org_id + ".  skipping opportunity " + id
       continue
     org = known_orgs[org_id]
-    s += getBaseEventRequiredFields(opp, org)
-    # event_type
-    s += SEP
-    s += "volunteering"
-    s += "\n"
+    opp_locations = opp.getElementsByTagName("location")
+    opp_times = opp.getElementsByTagName("dateTimeDuration")
+    for opptime in opp_times:
+      # event_time_range
+      # e.g. 2006-12-20T23:00:00/2006-12-21T08:30:00, in PST (GMT-8)
+      startDate = getTagValue(opptime, "startDate")
+      startTime = getTagValue(opptime, "startTime")
+      endDate = getTagValue(opptime, "endDate")
+      endTime = getTagValue(opptime, "endTime")
+      tz = getTagValue(opptime, "tzOlsonPath")
+      # e.g. 2006-12-20T23:00:00/2006-12-21T08:30:00, in PST (GMT-8)
+      if (startDate == ""):
+        startend = ""
+      elif (endDate == ""):
+        startend = cvtDateTimeToGoogleBase(startDate, startTime, tz)
+      else:
+        startend = cvtDateTimeToGoogleBase(startDate, startTime, tz)
+        startend += "/"
+        startend += cvtDateTimeToGoogleBase(endDate, endTime, tz)
+      for opploc in opp_locations:
+        recno = recno + 1
+        if debug:
+          s += "--- record %s\n" % (i)
+        s += RECORDSEP
+        s += outputLocationField(opploc, "location") + FIELDSEP
+        s += outputField("event_time_range", startend) + FIELDSEP
+        s += getBaseEventRequiredFields(opp, org)
+        s += getBaseOtherFields(opp, org)
+        s += getDirectMappedField(opp, org)
   return s
 
 
 if __name__ == "__main__":
   sys = __import__('sys')
   fp = __import__('parse_footprint')
+  #debug = True
+  #FIELDSEP = "\n"
   xmldoc = fp.ParseFootprintXML(sys.argv[1])
   print convertToGoogleBaseEventsType(xmldoc)
-
-#  for org in xmldoc.getElementsByTagName("Organization"):
-#    for node in org.childNodes:
-#      if node.firstChild != None:
-#        print node.tagName + "=" + node.firstChild.data
 
