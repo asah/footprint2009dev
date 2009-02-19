e198161c9f447bebf81153c232e3b45b14aeac7a
paul.rademacher <paul.rademacher@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-02-19 10:39:04 +0000

    Added basic pagination: Prev Page, Next Page. Still need to do goto-page-number, and properly determine total number of hits (to correctly calculate the max # of pages)

--- frontend/base_search.py
+++ frontend/base_search.py
@@ -27,29 +27,34 @@ import utils
 #   Docs: http://code.google.com/apis/base/docs/2.0/attrs-queries.html
 BASE_SNIPPETS_URL = 'http://www.google.com/base/feeds/snippets'
 
-def Search(query, location):
+
+def Search(query, location, start_index, num_results):
   location_param = '[location: @"%s" + 5mi]' % location
 #  link_param = '[link: idealist]'
   link_param = ''
   base_query = 'volunteer %s %s %s' % (query, location_param, link_param)
 
-  url_params = urllib.urlencode({'max-results': '10',
-                                 'bq': base_query })
+  if not start_index:
+    start_index = 1
+
+  url_params = urllib.urlencode({'max-results': num_results,
+                                 'bq': base_query,
+                                 'start-index': start_index })
   query_url = '%s?%s' % (BASE_SNIPPETS_URL, url_params)
 
+  result_set = searchresult.SearchResultSet(urllib.unquote(query_url),
+                                            query_url,
+                                            [])
   fetch_result = fetch(query_url)
   if fetch_result.status_code != 200:
-    return None
+    return result_set
 
   dom = minidom.parseString(fetch_result.content)
 
-  results = []
   for entry in dom.getElementsByTagName('entry'):
     url = entry.getElementsByTagName('link')[0].getAttribute('href')
     snippet = utils.GetXmlDomText(entry.getElementsByTagName('content')[0])
     title = utils.GetXmlDomText(entry.getElementsByTagName('title')[0])
-    results.append(searchresult.SearchResult(url, title, snippet))
+    result_set.results.append(searchresult.SearchResult(url, title, snippet))
 
-  return searchresult.SearchResultSet(urllib.unquote(query_url),
-                                      query_url,
-                                      results)
+  return result_set
--- frontend/search.py
+++ frontend/search.py
@@ -3,5 +3,5 @@
 
 import base_search
 
-def Search(query, location):
-  return base_search.Search(query, location)
+def Search(query, location, start_index, num_results):
+  return base_search.Search(query, location, start_index, num_results)
