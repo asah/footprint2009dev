f10fb69de21fd239b69ddaf784515800b0f4ab5e
doll.cassie <doll.cassie@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-02-19 00:03:04 +0000

    The images dir is now served. Moved getting user information into the util class. Added js escaped strings to the searchresult object.

--- frontend/searchresult.py
+++ frontend/searchresult.py
@@ -1,12 +1,22 @@
 # Copyright 2009 Google Inc.  All Rights Reserved.
 #
 
+import re
+
 class SearchResult(object):
   def __init__(self, url, title, snippet):
     self.url = url
     self.title = title
     self.snippet = snippet
+    # app engine does not currently support the escapejs filter in templates
+    # so we have to do it our selves for now
+    self.jsEscapedTitle = self.jsEscape(title)
+    self.jsEscapedSnippet = self.jsEscape(snippet)
 
+  def jsEscape(self, string):
+    # TODO: This escape method is overly agressive and is messing some snippets up.
+    # We only need to escape single and double quotes.  
+    return re.escape(string)
 
 class SearchResultSet(object):
   def __init__(self, query_url_unencoded, query_url_encoded, results):
