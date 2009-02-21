0170410f8043922c8f8d4c4b7463ebf51cd31ed6
paul.rademacher <paul.rademacher@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-02-21 02:12:06 +0000

    Moves inline JS to separate files.

---
+++ frontend/js/snippets_list.js
@@ -0,0 +1,39 @@
+google.friendconnect.container.loadOpenSocialApi({site: SITE_ID, onload: function() {}});
+
+function broadcastEvent(div, eventUrl, eventTitle, eventSnippet) {
+  opensocial.requestCreateActivity(opensocial.newActivity(
+      {title: '{{userDisplayName}} shared an event: <a href="' + eventUrl + '">' + eventTitle + '</a>',
+        body: eventSnippet}));
+  // TODO: This is a hack. We need to figure out the real ui here...
+  div.childNodes[1].src="images/broadcasticon-on.png";
+  div.setAttribute("onclick", "");
+}
+
+function shareEvent(div, eventUrl, eventTitle, eventSnippet) {
+  google.friendconnect.requestInvite("{{userDisplayName}} wants you to check out this event! "
+      + eventTitle + " " + eventUrl);
+}
+
+function addToCalendar(div, type, eventUrl, eventTitle, eventSnippet, eventDate, eventLocation) {
+  // TODO: Handle ical and outlook
+  var url;
+
+  if (type == 'GOOGLE') {
+    url = "http://www.google.com/calendar/event?action=TEMPLATE"
+        + "&text=" + eventTitle
+        + "&dates=" + (eventDate + "/" + eventDate)
+        + "&details=" + eventSnippet + "+" + eventUrl
+        + "&location=" + eventLocation;
+
+  } else if (type == 'YAHOO') {
+    url = "http://calendar.yahoo.com?v=60"
+        + "&ST=" + eventDate
+        + "&TITLE=" + eventTitle
+      //+ "&DUR=" + eventDuration TODO: Support duration once we have real dates
+        + "&VIEW=d"
+        + "&DESC=" + eventSnippet
+        + "&in_loc=" + eventLocation;
+  }
+
+  window.open(url, 'calendar');
+}
