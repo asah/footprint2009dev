6670e1c320ccd0c422bb8531f49801543785cf0f
adam.sah@gmail.com <adam.sah@gmail.com@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-02-21 01:51:14 +0000

    usaservice supported-- FP itself broken though...

--- datahub/xml_helpers.py
+++ datahub/xml_helpers.py
@@ -18,8 +18,27 @@ def getTagValue(entity, tag):
     return ""
   #print nodes[0].firstChild.data
   s = nodes[0].firstChild.data
+  s = escape(s).encode('UTF-8')
   s = re.sub(r'\n', r'\\n', s)
-  s = escape(s)
   return s
 
+def validateXML(xmldoc, known_elnames):
+  for node in xmldoc.childNodes:
+    if (node.nodeType == node.ELEMENT_NODE and
+        node.tagName not in known_elnames):
+      print "unknown tagName '"+node.tagName+"'"
+      # TODO: spellchecking...
+    validateXML(node, known_elnames)
 
+def simpleParser(s, known_elnames):
+  try:
+    xmldoc = minidom.parseString(s)
+    validateXML(xmldoc, known_elnames)
+    return xmldoc
+  except ExpatError, ee:
+    print "XML parsing error on line ", ee.lineno
+    lines = s.split("\n")
+    for i in range(ee.lineno - 3, ee.lineno + 3):
+      if i >= 0 and i < len(lines):
+        print "%6d %s" % (i+1, lines[i])
+    exit(0)
