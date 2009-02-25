df5553c72027c8c3cd8cc1ac1674b36827fcb3c4
adam.sah@gmail.com <adam.sah@gmail.com@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-02-25 09:24:06 +0000

    towards a better search experience...

--- frontend/views.py
+++ frontend/views.py
@@ -5,6 +5,7 @@ import cgi
 import datetime
 import os
 import urllib
+import logging
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
@@ -17,10 +18,11 @@ import search
 import urls
 import utils
 
-
 TEMPLATE_DIR = 'templates/'
 MAIN_PAGE_TEMPLATE = 'main_page.html'
+TEST_PAGEVIEWS_TEMPLATE = 'test_pageviews.html'
 SEARCH_RESULTS_TEMPLATE = 'search_results.html'
+SEARCH_RESULTS_DEBUG_TEMPLATE = 'search_results_debug.html'
 SEARCH_RESULTS_RSS_TEMPLATE = 'search_results.rss'
 SNIPPETS_LIST_TEMPLATE = 'snippets_list.html'
 SNIPPETS_LIST_RSS_TEMPLATE = 'snippets_list.rss'
@@ -36,42 +38,55 @@ def RenderTemplate(template_filename, template_values):
   return template.render(path, template_values)
 
 
+class TestPageViewsView(webapp.RequestHandler):
+  def get(self):
+    pagename = "testpage%s" % (self.request.get('pagename'))
+    pc = pagecount.IncrPageCount(pagename, 1)
+    template_values = pagecount.GetStats()
+    template_values['pagename'] = pagename
+    template_values['pageviews'] = pc
+    self.response.out.write(RenderTemplate(TEST_PAGEVIEWS_TEMPLATE,
+                                           template_values))
+
 class MainPageView(webapp.RequestHandler):
   def get(self):
     template_values = []
     self.response.out.write(RenderTemplate(MAIN_PAGE_TEMPLATE,
                                            template_values))
 
+def RunSearch(req):
+  return result_set
 
+# TODO: legacy consumer UI, to be removed
 class SearchView(webapp.RequestHandler):
   def get(self):
-    query = self.request.get('q')
-    location = self.request.get('loc')
+    args = self.request.arguments()
+    unique_args = {}
+    for arg in args:
+      allvals = self.request.get_all(arg)
+      unique_args[arg] = allvals[len(allvals)-1]
+    result_set = search.Search(unique_args)
+    template_values = {
+        'query_url_encoded': result_set.query_url_encoded,
+        'query_url_unencoded': result_set.query_url_unencoded,
+        'results': result_set.results,
+        'keywords': result_set.args["q"],
+        'location': result_set.args["vol_loc"],
+        'currentPage' : 'SEARCH'
+      }
+    self.response.out.write(RenderTemplate(SEARCH_RESULTS_TEMPLATE,
+                                           template_values))
 
-    start_index = utils.StringToInt(self.request.get('start')) or 1
-    if start_index < 1:
-      start_index = 1
-    num_results_requested = DEFAULT_NUM_RESULTS
-    is_first_page = (start_index <= num_results_requested)
-
-    # TODO(paul): need to determine total # of results
-
-    output = self.request.get('output')
-
-    result_set = search.Search(query, location,
-                               start_index, num_results_requested)
-
-    # TODO(paul): Fix this.  It needs to use total num results.
-    is_last_page = (len(result_set.results) < num_results_requested)
-
-    def BuildSearchUrl(start_index):
-      return ('%s?%s' %
-          (urls.URL_SEARCH, urllib.urlencode({'q': query,
-                                              'loc': location,
-                                              'start': start_index })))
-
-    prev_page_url = BuildSearchUrl(start_index - DEFAULT_NUM_RESULTS)
-    next_page_url = BuildSearchUrl(start_index + DEFAULT_NUM_RESULTS)
+class SearchAPIView(webapp.RequestHandler):
+  def get(self):
+    args = self.request.arguments()
+    unique_args = {}
+    for arg in args:
+      allvals = self.request.get_all(arg)
+      unique_args[arg] = allvals[len(allvals)-1]
+    if "output" not in unique_args:
+      unique_args["output"] = "rss"
+    result_set = search.Search(unique_args)
 
     user_info = utils.GetUserInfo()
     user_id = ""
@@ -82,24 +97,44 @@ class SearchView(webapp.RequestHandler):
 
     template_values = {
         'result_set': result_set,
-        'keywords': query,
-        'location': location,
         'currentPage' : 'SEARCH',
         'userId' : user_id,
         'userDisplayName' : user_display_name,
-        'is_first_page': is_first_page,
-        'is_last_page': is_last_page,
-        'prev_page_url': prev_page_url,
-        'next_page_url': next_page_url,
+
+        # TODO: remove this stuff...
+        'keywords': result_set.args["q"],
+        'location': result_set.args["vol_loc"],
+        'is_first_page': result_set.is_first_page,
+        'is_last_page': result_set.is_last_page,
+        'prev_page_url': result_set.prev_page_url,
+        'next_page_url': result_set.next_page_url,
       }
 
-    if output == "rss":
-      self.response.out.write(RenderTemplate(SEARCH_RESULTS_RSS_TEMPLATE,
-                                             template_values))
+    output = result_set.args["output"]
+    if output == "consumerui":
+      tpl = SEARCH_RESULTS_TEMPLATE
+    elif output == "rss":
+      tpl = SEARCH_RESULTS_RSS_TEMPLATE
+    elif output == "csv":
+      # TODO: implement SEARCH_RESULTS_CSV_TEMPLATE
+      tpl = SEARCH_RESULTS_RSS_TEMPLATE
+    elif output == "tsv":
+      # TODO: implement SEARCH_RESULTS_TSV_TEMPLATE
+      tpl = SEARCH_RESULTS_RSS_TEMPLATE
+    elif output == "xml":
+      # TODO: implement SEARCH_RESULTS_XML_TEMPLATE
+      tpl = SEARCH_RESULTS_XML_TEMPLATE
+    elif output == "rssdesc":
+      # TODO: implement SEARCH_RESULTS_RSSDESC_TEMPLATE
+      tpl = SEARCH_RESULTS_RSS_TEMPLATE
+    elif output == "html":
+      tpl = SEARCH_RESULTS_DEBUG_TEMPLATE
     else:
-      # html output
-      self.response.out.write(RenderTemplate(SEARCH_RESULTS_TEMPLATE,
-                                             template_values))
+      # TODO: implement SEARCH_RESULTS_ERROR_TEMPLATE
+      # TODO: careful about escapification/XSS
+      template_values["error"] = "no such output format."
+      tpl = SEARCH_RESULTS_RSS_TEMPLATE
+    self.response.out.write(RenderTemplate(tpl, template_values))
 
 class MyEventsView(webapp.RequestHandler):
   def get(self):
@@ -126,7 +161,6 @@ class MyEventsView(webapp.RequestHandler):
 
     self.response.out.write(RenderTemplate(MY_EVENTS_TEMPLATE,
                                            template_values))
-
 class PostView(webapp.RequestHandler):
   def get(self):
     template_values = {
@@ -134,3 +168,4 @@ class PostView(webapp.RequestHandler):
     }
 
     self.response.out.write(RenderTemplate(POST_TEMPLATE, template_values))
+
