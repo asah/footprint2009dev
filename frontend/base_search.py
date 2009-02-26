75fd20a36a5b1d4d093c46c7f00a205b649c92ec
adam.sah@gmail.com <adam.sah@gmail.com@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-02-26 18:48:13 +0000

    proper handling of base customer ID restrict, exposed as &base_customer=NNN minor cleanups

--- frontend/base_search.py
+++ frontend/base_search.py
@@ -22,11 +22,18 @@ import base_search
 import searchresult
 import utils
 
+def BaseArg(x):
+  return "base_" + x
+
 # note: many of the XSS and injection-attack defenses are unnecessary
 # given that the callers are also protecting us, but I figure better
 # safe than sorry, and defense-in-depth.
 def Search(args):
   base_query = ""
+
+  # TODO: injection attack on q
+  if "q" in args:
+    base_query += ' '+args["q"]
 
   # TODO: injection attacks in vol_loc
   if args["vol_loc"] != "":
@@ -39,33 +46,21 @@ def Search(args):
   if "backend" not in args:
     args["backends"] = "http://www.google.com/base/feeds/snippets"
 
-  #if "base_authorid" not in args:
-  #  args["base_authorid"] = 5663714
-  #else:
-  #  args["base_authorid"] = int(str(args["base_authorid"]))
+  if BaseArg("customer") not in args:
+    args[BaseArg("customer")] = 5663714;
+  base_query += ' [customer id: '+str(int(args[BaseArg("customer")]))+']'
 
-  # TODO: omg this is broken on so many levels, e.g. not a real attrib search
-  # e.g. should use author id instead of name, etc.
-  # TODO: injection attack on base_author
-  if "base_author" not in args:
-    base_query += '"footprint project"'
-  else:
-    base_query += '"'+args["base_author"]+'"'
+  if "num" not in args:
+    args["num"] = 10
+  if "start" not in args:
+    args["start"] = 1
 
-  # TODO: injection attack on q
-  if "q" in args:
-    base_query += ' '+args["q"]
-
-  for k in args:
-    logging.info("arg["+str(k)+"]="+str(args[k]))
-
+  #for k in args: logging.info("arg["+str(k)+"]="+str(args[k]))
   url_params = urllib.urlencode({'max-results': args["num"],
 				 'start-index': args["start"],
                                  'bq': base_query,
-                                 #'authorid': int(args["base_authorid"]),
                                  })
   query_url = '%s?%s' % (args["backends"], url_params)
-  logging.info("query_url="+query_url)
 
   result_set = searchresult.SearchResultSet(urllib.unquote(query_url),
                                             query_url,
