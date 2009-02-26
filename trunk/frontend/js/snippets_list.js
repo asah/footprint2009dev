fa00776e559198f73292c5703a26a5653c2ffb5d
matthew.blain@google.com <matthew.blain@google.com@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-02-26 19:59:59 +0000

    interest expression (i.e.. starring) of volunteer opportunities userinfo module for abstract userinfo memcache of queries and friendconnect user info test page for viewing some of the data.

--- trunk/frontend/js/snippets_list.js
+++ trunk/frontend/js/snippets_list.js
@@ -2,7 +2,7 @@ google.friendconnect.container.loadOpenSocialApi({site: SITE_ID, onload: functio
 
 function broadcastEvent(div, eventUrl, eventTitle, eventSnippet) {
   opensocial.requestCreateActivity(opensocial.newActivity(
-      {title: document.userDisplayName 
+      {title: document.userDisplayName
               + ' shared an event: <a href="' + eventUrl + '">' + eventTitle + '</a>',
         body: eventSnippet}));
   // TODO: This is a hack. We need to figure out the real ui here...
@@ -38,4 +38,30 @@ function addToCalendar(div, type, eventUrl, eventTitle, eventSnippet, eventDate,
   }
 
   window.open(url, 'calendar');
+}
+
+function expressInterest(div, eventUrl, interest) {
+  if (div.interest != undefined) {
+    interest = div.interest;
+  }
+  interest = 0 + interest;
+  // hack!
+  newinterest = 1;
+  if (interest > 0) {
+    newinterest = 0;
+  }
+  div.interest = newinterest;
+
+  // This escaping code is unsafe!
+  div.childNodes[1].src='/test/interest?i=' + newinterest
+                        + '&oid=' + escape(eventUrl)
+                        + '&zx=' + Math.random();
+  function fixit() {
+    if (newinterest) {
+      div.childNodes[1].src='images/markers/red-dot.png';
+    } else {
+      div.childNodes[1].src='images/markers/red.png';
+    }
+  }
+  window.setTimeout(fixit, 10);
 }
