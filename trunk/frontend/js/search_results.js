f9bfd9f00db132de99860b7a883e35db2a11e62e
oansaldi@gmail.com <oansaldi@gmail.com@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-02-27 15:48:57 +0000

    Calendar widget + couple of util functions.

--- trunk/frontend/js/search_results.js
+++ trunk/frontend/js/search_results.js
@@ -1,5 +1,39 @@
 var map;
 var workQueue = new WorkQueue();
+
+workQueue.addCallback(function() {
+  var events = {
+    '20090228': 'something',
+    '20090301': 'something else',
+    '20090305': 'one more thing',
+    '20090315': 'and one last thing'
+  };
+  var element = el('calendar');
+  var calendar = new footprint.Calendar(element, events);
+  calendar.render();
+
+  function nextMonth() { calendar.nextMonth(); }
+  function previousMonth() { calendar.previousMonth(); }
+  
+  forEachElementOfClass('calendar_month_previous', function(e) {
+    addEventListener(e, 'click', previousMonth);
+  }, element);
+  forEachElementOfClass('calendar_month_next', function(e) {
+    addEventListener(e, 'click', nextMonth);
+  }, element);
+  
+  function unregisterEventListeners() {
+    forEachElementOfClass('calendar_month_previous', function(e) {
+      removeEventListener(e, 'click', previousMonth);
+    }, element);
+    forEachElementOfClass('calendar_month_next', function(e) {
+      removeEventListener(e, 'click', nextMonth);
+    }, element);
+  }
+  // TODO(oansaldi): cleanup event listeners on unload
+  // unloadWorkQueue.addCallback(unregisterEventListeners);
+});
+
 function mapApiLoadComplete() {
   var queryParams = GetQueryParams();
   if (queryParams['loc']) {
