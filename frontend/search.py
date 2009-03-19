29cf15ff2633d4d85d53ae6f12bd9230edabbbc7
blake.schwendiman <blake.schwendiman@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-03-19 16:15:43 +0000

    Updated API to respect the 'num' parameter and limit the size of the result set.

--- frontend/search.py
+++ frontend/search.py
@@ -96,6 +96,7 @@ def search(args):
   scoring.score_results_set(result_set, args)
   result_set.apply_post_search_filters(args)
   result_set.dedup()
+  result_set.limit_to(args["num"])
   
   result_set.is_first_page = (start_index == 1)
   # TODO: detect last page
