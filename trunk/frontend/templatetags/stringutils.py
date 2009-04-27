73de1bffd812d8f76a7cc6fdca0e24d0bc7bdfc1
paul.rademacher <paul.rademacher@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-04-27 21:31:34 +0000

    bugfix: undefined variable in python truncate filter

--- trunk/frontend/templatetags/stringutils.py
+++ trunk/frontend/templatetags/stringutils.py
@@ -7,11 +7,11 @@ from google.appengine.ext import webapp
 def truncate_chars(value, max_length):
   if len(value) > max_length:
     truncated_value = value[:max_length]
+    ellipsis = ' ...'
     if value[max_length + 1] != ' ':
       # TODO: Make sure that only whitespace in the data records
       #     is ascii spaces.
       right_index = truncated_value.rfind(' ')
-      ellipsis = ' ...'
       MAX_CHARS_TO_CLIP = 40
       if right_index < max_length - MAX_CHARS_TO_CLIP:
         right_index = max_length - MAX_CHARS_TO_CLIP
