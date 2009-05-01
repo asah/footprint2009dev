db3c4196cb3db2c9c0d48da9a7623743170f2806
oansaldi <oansaldi@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-05-01 10:13:05 +0000

    Improved maps markers.

--- trunk/frontend/js/map.js
+++ trunk/frontend/js/map.js
@@ -15,57 +15,71 @@ limitations under the License.
 
 
 /* A GMap-management object.  Simplifies viewpoint and marker geocoding. */
-function SimpleMap(div) {
+SimpleMap = function(div) {
+  if (!GBrowserIsCompatible()) {
+    return;
+  }
   this.div_ = div;
   this.defaultZoom_ = 10;
 
-  if (GBrowserIsCompatible()) {
-    this.map = new GMap2(div);
+  this.map = new GMap2(div);
 
-    var lat = 40, lon = -100, zoom = 3;
-    try {
-      lat = google.loader.ClientLocation.latitude;
-      lon = google.loader.ClientLocation.longitude;
-      zoom = this.defaultZoom_;
-    } catch (err) {}
+  var lat = 40, lon = -100, zoom = 3;
+  try {
+    lat = google.loader.ClientLocation.latitude;
+    lon = google.loader.ClientLocation.longitude;
+    zoom = this.defaultZoom_;
+  } catch (err) {}
 
-    this.map.setCenter(new GLatLng(lat, lon), zoom);
-    this.map.enableContinuousZoom();
-    this.geocoder_ = new GClientGeocoder();
-    this.map.addControl(new GSmallZoomControl());
+  this.map.setCenter(new GLatLng(lat, lon), zoom);
+  this.map.enableContinuousZoom();
+  this.geocoder_ = new GClientGeocoder();
+  this.map.addControl(new GSmallZoomControl());
 
-    if (false) {
-      this.icon_ = new GIcon(G_DEFAULT_ICON, "/images/markers/red.png");
-      this.icon_.iconSize.width = 32;
-      this.icon_.iconSize.height = 32;
-      this.icon_.iconAnchor.x = 15;
-      this.icon_.iconAnchor.y = 31;
-    }
-    return this;
-  } else {
-    return null;
-  }
-}
+  var icon = new GIcon();
+  icon.image = 'http://www.google.com/mapfiles/marker_midblue.png';
+  icon.shadow = 'http://www.google.com/mapfiles/shadow-mid.png';
+  icon.iconSize = new GSize(16, 27);
+  icon.shadowSize = new GSize(16, 28);
+  icon.iconAnchor = new GPoint(8, 27);
+  icon.infoWindowAnchor = new GPoint(5, 1);
+  this.defaultIcon_ = icon;
+};
 
 SimpleMap.prototype.setCenter = function(latLng) {
+  if (!GBrowserIsCompatible()) {
+    return;
+  }
   this.map.setCenter(latLng, this.defaultZoom_);
-}
+};
 
 SimpleMap.prototype.setCenterGeocode = function(locationString) {
+  if (!GBrowserIsCompatible()) {
+    return;
+  }
   var me = this;
   this.geocoder_.getLatLng(locationString, function(latLng) {
     if (latLng) {
       me.setCenter(latLng);
     }
   });
-}
+};
 
-SimpleMap.prototype.addMarker = function(lat, lng) {
+SimpleMap.prototype.addMarker = function(lat, lng, opt_suffix) {
+  if (!GBrowserIsCompatible()) {
+    return;
+  }
   var latLng = new GLatLng(lat, lng);
-  this.map.addOverlay(new GMarker(latLng, this.icon_));
-}
+  var url = 'http://www.google.com/mapfiles/marker_midblue' +
+      [opt_suffix === undefined ? '' : opt_suffix] +
+      '.png';
+  this.map.addOverlay(new GMarker(latLng, new GIcon(this.defaultIcon_, url)));
+};
 
 SimpleMap.prototype.addMarkerGeocode = function(locationString) {
+  if (!GBrowserIsCompatible()) {
+    return;
+  }
   var me = this;
   // TODO(paul): Use .getLocations() method instead, to get accuracy rating.
   this.geocoder_.getLatLng(locationString, function(latLng) {
@@ -73,14 +87,17 @@ SimpleMap.prototype.addMarkerGeocode = function(locationString) {
       me.addMarker(latLng.lat(), latLng.lng());
     }
   });
-}
+};
 
 /** callback is a function that accepts a GLatLng and an accuracy.
  */
 SimpleMap.prototype.geocode = function(locationString, callback) {
+  if (!GBrowserIsCompatible()) {
+    return;
+  }
   var me = this;
   this.geocoder_.getLatLng(locationString, function(latLng) {
     // TODO: implement accuracy.
     callback(latLng, 0);
   });
-}
+};
