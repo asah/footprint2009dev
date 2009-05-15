077bae0d7460e2788d45d5b115937569db641c58
manzoid@gmail.com <manzoid@gmail.com@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-05-15 19:52:39 +0000

    Run base js through gadget proxy and suprfetch.

--- frontend/views.py
+++ frontend/views.py
@@ -225,6 +225,8 @@ class home_page_view(webapp.RequestHandler):
     template_values = {
       'user' : user,
       'current_page' : 'HOMEPAGE',
+      'host' : self.request.host_url,
+      'js_debug' : self.request.get('js_debug'),
     }
     self.response.out.write(render_template(HOMEPAGE_TEMPLATE,
                                            template_values))
@@ -238,6 +240,8 @@ class consumer_ui_search_view(webapp.RequestHandler):
         'result_set': {},
         'current_page' : 'SEARCH',
         'is_main_page' : True,
+        'host' : self.request.host_url,
+        'js_debug' : self.request.get('js_debug'),
       }
     # Retrieve the user-specific information for the search result set.
     user = userinfo.get_user(self.request)
@@ -359,6 +363,8 @@ class ui_snippets_view(webapp.RequestHandler):
             result_set.clip_start_index + len(result_set.clipped_results),
         'display_nextpage_link' : result_set.has_more_results,
         'view_url': self.request.url,
+        'host' : self.request.host_url,
+        'js_debug' : self.request.get('js_debug'),
         'friends' : view_data['friends'],
         'friends_by_event_id_js': view_data['friends_by_event_id_js'],
         'query_param_q' : unique_args.get(api.PARAM_Q, None),
@@ -432,6 +438,8 @@ class ui_my_snippets_view(webapp.RequestHandler):
       template_values = {
           'current_page' : 'MY_EVENTS',
           'view_url': self.request.url,
+          'host' : self.request.host_url,
+          'js_debug' : self.request.get('js_debug'),
           'user' : user_info,
           'result_set': my_events_gbase_result_set,
           'has_results' : len(my_events_gbase_result_set.clipped_results) > 0,
@@ -470,6 +478,7 @@ class my_events_view(webapp.RequestHandler):
 
     template_values = {
         'current_page' : 'MY_EVENTS',
+        'host' : self.request.host_url,
     }
     load_userinfo_into_dict(user_info, template_values)
 
