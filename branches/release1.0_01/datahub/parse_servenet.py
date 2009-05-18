e986af4a8dddaa5b8b721639d2b249ead03feafc
paul.rademacher <paul.rademacher@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-05-18 22:38:18 +0000

    Release branch.

---
+++ branches/release1.0_01/datahub/parse_servenet.py
@@ -0,0 +1,164 @@
+# Copyright 2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+parser for SERVEnet
+"""
+
+import xml_helpers as xmlh
+import re
+from datetime import datetime
+
+ORGS = {}
+ORGIDS = {}
+MAX_ORGID = 0
+
+def register_org(orgname, orgstr):
+  """register the organization info, for lookup later."""
+  global MAX_ORGID
+  if orgname in ORGIDS:
+    return ORGIDS[orgname]
+  MAX_ORGID = MAX_ORGID + 1
+  orgstr = '<Organization>'
+  orgstr += '<organizationID>%d</organizationID>' % (len(ORGIDS))
+  orgstr += '<nationalEIN></nationalEIN>'
+  orgstr += '<name>%s</name>' % (orgname)
+  orgstr += '<missionStatement></missionStatement>'
+  orgstr += '<description></description>'
+  orgstr += '<location>'
+  orgstr += xmlh.output_val("city", "")
+  orgstr += xmlh.output_val("region", "")
+  orgstr += xmlh.output_val("postalCode", "")
+  orgstr += '</location>'
+  orgstr += '<organizationURL></organizationURL>'
+  orgstr += '<donateURL></donateURL>'
+  orgstr += '<logoURL></logoURL>'
+  orgstr += '<detailURL></detailURL>'
+  orgstr += '</Organization>'
+  ORGS[MAX_ORGID] = orgstr
+  ORGIDS[orgname] = MAX_ORGID
+  return MAX_ORGID
+
+# pylint: disable-msg=R0915
+def parse(instr, maxrecs, progress):
+  """return FPXML given servenet data"""
+
+  known_elnames = [
+    'Abstract', 'Categories', 'Category', 'CategoryID', 'Country', 'DateListed',
+    'Description', 'DetailURL', 'Duration', 'DurationQuantity', 'DurationUnit',
+    'EndDate', 'KeyWords', 'LocalID', 'Location', 'LocationClassification',
+    'LocationClassificationID', 'LocationClassifications', 'Locations',
+    'LogoURL', 'Name', 'OpportunityDate', 'OpportunityDates', 'OpportunityType',
+    'OpportunityTypeID', 'SponsoringOrganization', 'SponsoringOrganizations',
+    'StartDate', 'StateOrProvince', 'Title', 'VolunteerOpportunity',
+    'ZipOrPostalCode' ]
+
+  numorgs = numopps = 0
+
+  instr = re.sub(r'<(/?db):', r'<\1_', instr)
+  opps = re.findall(r'<VolunteerOpportunity>.+?</VolunteerOpportunity>',
+                    instr, re.DOTALL)
+  volopps = ""
+  for i, oppstr in enumerate(opps):
+    if progress and i > 0 and i % 250 == 0:
+      print str(datetime.now())+": ", i, " opportunities processed."
+    if (maxrecs > 0 and i > maxrecs):
+      break
+    xmlh.print_rps_progress("opps", progress, i, maxrecs)
+
+    item = xmlh.simple_parser(oppstr, known_elnames, progress=False)
+
+    # SponsoringOrganization/Name -- fortunately, no conflicts
+    # but there's no data except the name
+    orgname = xmlh.get_tag_val(item, "Name")
+    orgid = register_org(orgname, orgname)
+
+    # logoURL -- sigh, this is for the opportunity not the org
+    volopps += '<VolunteerOpportunity>'
+    volopps += xmlh.output_val('volunteerOpportunityID', str(i))
+    volopps += xmlh.output_val('sponsoringOrganizationID', str(orgid))
+    volopps += xmlh.output_node('volunteerHubOrganizationID', item, "LocalID")
+    volopps += xmlh.output_node('title', item, "Title")
+    volopps += xmlh.output_node('abstract', item, "Abstract")
+    volopps += xmlh.output_node('description', item, "Description")
+    volopps += xmlh.output_node('detailURL', item, "DetailURL")
+    volopps += xmlh.output_val('volunteersNeeded', "-8888")
+
+    oppdates = item.getElementsByTagName("OpportunityDate")
+    if oppdates.length > 1:
+      print datetime.now(), \
+          "parse_servenet.py: only 1 OpportunityDate supported."
+      #return None
+      oppdate = oppdates[0]
+    elif oppdates.length == 0:
+      oppdate = None
+    else:
+      oppdate = oppdates[0]
+    volopps += '<dateTimeDurations><dateTimeDuration>'
+
+    if oppdate:
+      volopps += xmlh.output_val('openEnded', 'No')
+      volopps += xmlh.output_val('duration', 'P%s%s' % 
+                                (xmlh.get_tag_val(oppdate, "DurationQuantity"),
+                                 xmlh.get_tag_val(oppdate, "DurationUnit")))
+      volopps += xmlh.output_val('commitmentHoursPerWeek', '0')
+      volopps += xmlh.output_node('startDate', oppdate, "StartDate")
+      volopps += xmlh.output_node('endDate', oppdate, "EndDate")
+    else:
+      volopps += xmlh.output_val('openEnded', 'Yes')
+      volopps += xmlh.output_val('commitmentHoursPerWeek', '0')
+    volopps += '</dateTimeDuration></dateTimeDurations>'
+
+    volopps += '<locations>'
+    opplocs = item.getElementsByTagName("Location")
+    for opploc in opplocs:
+      volopps += '<location>'
+      volopps += xmlh.output_node('region', opploc, "StateOrProvince")
+      volopps += xmlh.output_node('country', opploc, "Country")
+      volopps += xmlh.output_node('postalCode', opploc, "ZipOrPostalCode")
+      volopps += '</location>'
+    volopps += '</locations>'
+    volopps += '<categoryTags/>'
+    volopps += '</VolunteerOpportunity>'
+    numopps += 1
+    
+  # convert to footprint format
+  outstr = '<?xml version="1.0" ?>'
+  outstr += '<FootprintFeed schemaVersion="0.1">'
+  outstr += '<FeedInfo>'
+  # TODO: assign provider IDs?
+  outstr += xmlh.output_val('providerID', '114')
+  outstr += xmlh.output_val('providerName', 'servenet')
+  outstr += xmlh.output_val('feedID', 'servenet')
+  outstr += xmlh.output_val('createdDateTime', xmlh.current_ts())
+  outstr += xmlh.output_val('providerURL', 'http://www.servenet.org/')
+  outstr += xmlh.output_val('description', 'servenet')
+  # TODO: capture ts -- use now?!
+  outstr += '</FeedInfo>'
+
+  # hardcoded: Organization
+  outstr += '<Organizations>'
+  for key in ORGS:
+    outstr += ORGS[key]
+    numorgs += 1
+  outstr += '</Organizations>'
+  outstr += '<VolunteerOpportunities>'
+  outstr += volopps
+  outstr += '</VolunteerOpportunities>'
+  outstr += '</FootprintFeed>'
+
+  #outstr = re.sub(r'><([^/])', r'>\n<\1', outstr)
+  return outstr, numorgs, numopps
+
+
---
+++ branches/release1.0_01/frontend/scoring.py
@@ -0,0 +1,105 @@
+# Copyright 2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+toss all the scoring code into one place (rather than a class file)
+because scoring tends to get complex quickly.
+"""
+
+from datetime import datetime
+import logging
+import math
+
+import view_helper
+
+def compare_scores(val1, val2):
+  """helper function for sorting."""
+  diff = val2.score - val1.score
+  if (diff > 0):
+    return 1
+  if (diff < 0):
+    return -1
+  return 0
+
+def score_results_set(result_set, args):
+  """sort results by score, and for each, set .score, .scorestr, .score_notes"""
+  logging.debug(str(datetime.now())+": score_results_set(): start")
+  idlist = map(lambda x: x.item_id, result_set.results)
+  # handle rescoring on interest weights
+  others_interests = view_helper.get_interest_for_opportunities(idlist)
+  total_results = float(len(result_set.results))
+  for i, res in enumerate(result_set.results):
+    score_by_base_rank = (total_results - i)/total_results
+    score = score_by_base_rank
+    score_notes = "  GBase relevance score=" + str(score_by_base_rank)
+
+    # TODO: match on start time, etc.
+    delta = res.startdate - datetime.now()
+    if delta.days <= 0:
+      # good luck joining event this soon
+      # also avoids divide-by-zero and dates in the past
+      date_dist_multiplier = .0001
+    else:  
+      date_dist_multiplier = 1.0/(delta.days + (delta.seconds/(24 * 3600)))
+
+    if (("lat" not in args) or args["lat"] == "" or
+        ("long" not in args) or args["long"] == "" or
+         res.latlong == ""):
+      geo_dist_multiplier = 0.5
+    else:
+      # TODO: error in the DB, we're getting same geocodes for everything
+      lat, lng = res.latlong.split(",")
+      latdist = float(lat) - float(args["lat"])
+      lngdist = float(lng) - float(args["long"])
+      # keep one value to right of decimal
+      delta_dist = latdist*latdist + lngdist*lngdist
+      logging.debug("qloc=%s,%s - listing=%g,%g - dist=%g,%g - delta = %g" %
+                   (args["lat"], args["long"], float(lat), float(lng),
+                    latdist, lngdist, delta_dist))
+      # reasonably local
+      if delta_dist > 0.025:
+        delta_dist = 0.9 + delta_dist
+      else:
+        delta_dist = delta_dist / (0.025 / 0.9)
+      if delta_dist > 0.999:
+        delta_dist = 0.999
+      geo_dist_multiplier = 1.0 - delta_dist
+
+    interest = -1
+    if res.item_id in others_interests:
+      interest = others_interests[res.item_id]
+    elif "test_stars" in args:
+      interest = i % 6
+
+    score *= geo_dist_multiplier
+    score_notes += "  geo multiplier=" + str(geo_dist_multiplier)
+
+    if interest >= 0:
+      # TODO: remove hocus-pocus math
+      interest_weight = (math.log(interest+1.0)/math.log(6.0))**3
+      score *= interest_weight
+      score_notes += "  "+str(interest)+"-stars="+str(interest_weight)
+
+    score *= date_dist_multiplier
+    score_notes += "  startdate multiplier=" + str(date_dist_multiplier)
+    score_notes += "\n"
+    score_notes += "  days delta=" + str(delta.days)
+    score_notes += "  start=" + str(res.startdate)
+    score_notes += "  now=" + str(datetime.now())
+    score_notes += "  delta.seconds=" + str(delta.seconds)
+    res.set_score(score, score_notes)
+
+  result_set.results.sort(cmp=compare_scores)
+  logging.debug(str(datetime.now())+": score_results_set(): done")
+
