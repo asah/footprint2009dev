2f2c99e0440a24de0902f98fe7d11f0081effe59
paul.rademacher <paul.rademacher@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-05-19 22:43:07 +0000

    Enable Facebook support on allforgood.org. Private keys (FB "Application Secret") are not submitted into SVN

--- trunk/frontend/deploy.py
+++ trunk/frontend/deploy.py
@@ -17,10 +17,18 @@ Data needed for live vs dev deployment.
 """
 
 import os
+import private_keys
 
 from google.appengine.ext import webapp
 
 PRODUCTION_DOMAIN = 'allforgood.org'
+
+PRODUCTION_MAPS_API_KEY = 'ABQIAAAAHtEBbyenR4BaYGl54_p0fRQu5fCZl1K7T-61hQb7PrEsg72lpRQbhbBcd0325oSLzGUQxP7Nz9Rquw'
+DEFAULT_MAPS_API_KEY = 'ABQIAAAAHtEBbyenR4BaYGl54_p0fRRlOb26qSyU154aZeLwOrF4C7-DphT-k84KU2QtDbk5G77Rqt1x2njBTQ'
+
+# These are the public Facebook API keys.
+DEFAULT_FACEBOOK_KEY = 'df68a40a4a90d4495ed03f920f16c333'
+PRODUCTION_FACEBOOK_KEY = '628524bbaf79da8a8a478e5ef49fb84f'
 
 def is_production_site():
   http_host = os.environ.get('HTTP_HOST')
@@ -31,6 +39,21 @@ def load_standard_template_values(template_values):
   #     devt on personal servers.
 
   if is_production_site():
-    template_values['maps_api_key'] = 'ABQIAAAAHtEBbyenR4BaYGl54_p0fRQu5fCZl1K7T-61hQb7PrEsg72lpRQbhbBcd0325oSLzGUQxP7Nz9Rquw'
+    template_values['maps_api_key'] = PRODUCTION_MAPS_API_KEY
+    template_values['facebook_key'] = PRODUCTION_FACEBOOK_KEY
   else:
-    template_values['maps_api_key'] = 'ABQIAAAAHtEBbyenR4BaYGl54_p0fRRlOb26qSyU154aZeLwOrF4C7-DphT-k84KU2QtDbk5G77Rqt1x2njBTQ'
+    template_values['maps_api_key'] = DEFAULT_MAPS_API_KEY
+    template_values['facebook_key'] = DEFAULT_FACEBOOK_KEY
+
+
+def get_facebook_key():
+  if is_production_site():
+    return PRODUCTION_FACEBOOK_KEY
+  else:
+    return DEFAULT_FACEBOOK_KEY
+
+def get_facebook_secret():
+  if is_production_site():
+    return private_keys.PRODUCTION_FACEBOOK_SECRET
+  else:
+    return private_keys.DEFAULT_FACEBOOK_SECRET
