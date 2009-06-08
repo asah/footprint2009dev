6c18ce1008f5942dbc354973c919f4b5c6ca99d3
adam.sah <adam.sah@e77c8ab2-f1dd-11dd-a295-0b7b0935bf94>
2009-06-08 21:34:42 +0000

    the big latency checkin.  fixes issue 131, issue 333 and issue 335
    views.py:
     - added optimize_page_speed(): improved algorithm for deciding whether
       development vs. production mode... better but not perfect yet.
     - get_default_template_values(): remove duplicate code for setting up
       std template values, also ensures that stuff like optimize_page is
       always available.
    
    *.html:
     - misc cleanups for HTML/CSS validation.
     - a lot more needs to be done-- I did the super easy stuff, which
       was making it hard to debug the rest of this CL.
    
    base.html:
     - gmodules/suprfetch turned on, for both CSS and JavaScript.
    
    base_min.html:
     - chartbeat disabled until we get clearance-- see comment in the code.
    
    deploy.py:
     - better key handling for non-prod and local sites
    
    homepage.js/homepage.html:
     - now a stateless bunch of function decls, included on every page
       (not just homepage.html)-- this is trivial overhead, but makes
       development easier and improves gmodules/suprfetch cache hitrate.
     - AJAX query call moved, so it runs before login bar, i.e. fires
       before Facebook/FriendConnect calls.
    
    
    note: checking in, to start getting help from other people.  Testing
       so far has been limited to:
     - the homepage, search results page and /about (i.e. a static page);
       I haven't tested moderator, admin, etc.
     - only on some browsers-- I suspect IE6 is broken.
     - open issue: top and side "glow"s are missing

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -13,54 +13,55 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-if (getDefaultLocation().displayLong) {
-  el('mini_with_location').style.display = '';
-} else {
-  el('mini_without_location').style.display = '';
-  el('location_form').style.display = '';
-}
-
-var defaultLocation = getDefaultLocation().displayLong || '';
-setInputFieldValue(el('location'), defaultLocation);
-
-el('more_link').href = 'javascript:submitForm("");void(0);';
-
 function goPopular(index) {
   setInputFieldValue(el('keywords'), popularSearches[index]);
   submitForm('');
 }
 
-// Populate the popular searches list.
-for (var i = 0; i < popularSearches.length; i++) {
-  var href = 'javascript:goPopular(' + i + ');void(0);';
-  var html = '<a href="' + href + '">' +
-      popularSearches[i] + '<' + '\a>';
-  el('popular_list').innerHTML += html + '<br>';
-}
-
-var vol_loc_term;
-if (defaultLocation != '') {
-  vol_loc_term = '&vol_loc=' + defaultLocation;
-} else {
-  // TODO: Make these the defaults inside search.py/base_search.py.
-  //       Will then need to test that "No Results" message doesn't
-  //       show "USA".
-  vol_loc_term = '&vol_loc=USA&vol_dist=1500';
-}
-var url = '/ui_snippets?start=0&num=4&minimal_snippets_list=1' + vol_loc_term;
-
-
-setTextContent(el('location_text'), defaultLocation);
-
-jQuery.ajax({
-      url: url,
-      async: true,
-      dataType: 'html',
-      error: function(){},
-      success: function(data){
-        if (data.length > 10) {  // Check if data is near-empty.
-          el('snippets').innerHTML = data;
-          el('more').style.display = '';
+/* requires that google /jsapi has been loaded... */
+var defaultLocation = getDefaultLocation().displayLong || '';
+function runSnippetsQuery() {
+  var vol_loc_term;
+  if (defaultLocation != '') {
+    vol_loc_term = '&vol_loc=' + defaultLocation;
+  } else {
+    /* TODO: Move defaults to search.py/base_search.py, then
+       test that "No Results" message doesn't show "USA". */
+    vol_loc_term = '&vol_loc=USA&vol_dist=1500';
+  }
+  var url = '/ui_snippets?start=0&num=4&minimal_snippets_list=1' + vol_loc_term;  
+  jQuery.ajax({
+        url: url,
+        async: true,
+        dataType: 'html',
+        error: function(){},
+        success: function(data){
+          if (data.length > 10) {  // Check if data is near-empty.
+            el('snippets').innerHTML = data;
+            el('more').style.display = '';
+          }
         }
-      }
-    });
+      });
+}
+
+function renderHomepage() {
+  // render homepage elements
+  setInputFieldValue(el('location'), defaultLocation);
+  if (defaultLocation != '') {
+    el('mini_with_location').style.display = '';
+  } else {
+    el('mini_without_location').style.display = '';
+    el('location_form').style.display = '';
+  }
+  
+  el('more_link').href = 'javascript:submitForm("");void(0);';
+  
+  // Populate the popular searches list.
+  for (var i = 0; i < popularSearches.length; i++) {
+    var href = 'javascript:goPopular(' + i + ');void(0);';
+    var html = '<a href="' + href + '">' +
+        popularSearches[i] + '<' + '\a>';
+    el('popular_list').innerHTML += html + '<br>';
+  }
+  setTextContent(el('location_text'), defaultLocation);  
+}
